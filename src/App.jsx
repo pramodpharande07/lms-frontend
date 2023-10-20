965e20f1c96053709a2cbdec0a1b2bcff2bc97ed
pramodpharande07 <pramodpharande2113@gmail.com>
2023-10-20 21:12:18 +0530

    added page not found

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Footer from "./components/Footer";
 import Homelayout from "./Layouts/Homelayout";
 import HomePage from "./Pages/HomePage";
 import AboutUs from "./Pages/AboutUs";
+import NotFound from "./Pages/NotFound";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/about" element={<AboutUs />}></Route>
+        <Route path="/*" element={<NotFound />}></Route>
         
       </Routes>
     </>
