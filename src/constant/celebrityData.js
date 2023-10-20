49f247ec835efb0ed02f75718a20581413d80b1f
pramodpharande07 <pramodpharande2113@gmail.com>
2023-10-20 20:33:24 +0530

    cleaned up our about us page

---
+++ src/constant/celebrityData.js
@@ -0,0 +1,43 @@
+import apj from "../assets/Images/apj.png";
+import billgates from "../assets/Images/billgates.png";
+import nelson from "../assets/Images/nelson.jpg";
+import steav from "../assets/Images/steav.png";
+import nambi from "../assets/Images/nambi.jpg"
+
+
+export const celebrities = [
+  {
+   title: "Abdul Kalam",
+   description: "In this world, fear has no place.",
+   image: apj,
+   slideNumber: 1
+ },
+
+ {
+   title: "Bill Gates" ,
+   description: "If you can't make it good, at least make it look good.",
+   image: billgates,
+   slideNumber: 2
+ },
+
+ {
+   title: "Nelson Mandela",
+   description: "You can never have an impact on society if you have not changed yourself." ,
+   image: nelson,
+   slideNumber: 3
+ },
+
+ {
+   title: "Steav Jobs",
+   description: "The people who are crazy enough to think they can change the world are the ones who do.",
+   image: steav,
+   slideNumber: 4
+ },
+ {
+  title: "Nambi Narayanan",
+  description: "Dream isn't that you see while resting something doesn't allow to rest",
+  image: nambi,
+  slideNumber: 5
+}
+]
+ 
